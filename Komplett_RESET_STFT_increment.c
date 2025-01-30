c9d7efcd881bf722898eda7ea5ef0c55ec7d8242
bufl1011 <165918683+bufl1011@users.noreply.github.com>
2025-01-30 15:13:21 +0100

    last_update

--- Komplett_RESET_STFT_increment.c
+++ Komplett_RESET_STFT_increment.c
@@ -7,7 +7,7 @@
 #define N 512
 #define BUFFER_SIZE (2 * N)
 #define NUM_NOISE_FRAMES 200 // Anzahl der Rauschframes
-#define HOP_SIZE N/2     // hop size (overlap)
+#define HOP_SIZE 256      // hop size (overlap) 400
 
 
 float32_t fft_input[N];
@@ -20,11 +20,15 @@ float32_t P_clean[N / 2];       // Geschätzte saubere Leistung
 double fft_freqs[N / 2];        // Frequenzen
 arm_rfft_fast_instance_f32 fft_instance;
 float hamming_window[N];
-float previous_ifft_output[N] = {0};  // Ring buffer for overlap-add
 
 uint32_t audio_ring_buffer[BUFFER_SIZE] = {0};
+uint32_t out_ring_buffer[BUFFER_SIZE]={0};
+float32_t overlap_buffer[N-HOP_SIZE]={0};
+
 int wrt_buf_ind = 0;
 int rd_buf_ind = 0;
+int wrt_out_ind = 0;
+int rd_out_ind  = 0;
 
 float32_t centroid = 10000.0f;
 float32_t spread = 0.0f;
@@ -38,20 +42,17 @@ int noise_cycle = 0;          // Noise cycle counter
 float notch_coeff;
 float prev_input = 0.0f, prev_output = 0.0f;
 // LMS-Filter Variablen
-#define STEP_SIZE 0.000081f //0.008f
-#define NOTCH_BANDWIDTH 20.0f
+#define STEP_SIZE 0.1f //0.008f
+#define NOTCH_BANDWIDTH 10.0f
 float a_hat = 0.0f;  // Fourier-Koeffizient für Cosinus
 float b_hat = 0.0f;  // Fourier-Koeffizient für Sinus
 float omega;         // Normalisierte Winkelgeschwindigkeit
 float audiooutput[N];
 // Wiener
 float32_t H[N / 2];     
-float32_t beta = 100;        // Wiener-Filter-Gewinn
-float32_t hamming_normalization = 0.0f;
-
-float32_t P_sig = 0; 
-
-
+float32_t beta = 100;        
+float32_t alpha = 1; 
+float32_t noise_increment = 1; 
 
 void process_audio();
 float compute_centroid(double *fft_freqs, float32_t *fft_power, int size);
@@ -65,7 +66,6 @@ int main(void) {
     platform_init(BAUDRATE, SAMPLERATE, line_in, dma, DSTC_I2S_HANDLER_CH0, DSTC_I2S_HANDLER_CH1);
     Uart0Init(115200);
 
-    GpioInit();
 
     if (arm_rfft_fast_init_f32(&fft_instance, N) != ARM_MATH_SUCCESS) {
         debug_printf("FFT initialization failed!\n");
@@ -76,14 +76,6 @@ int main(void) {
         fft_freqs[i] = (double)i * SAMPLERATE / N;
     }
     generate_hamming_window(hamming_window, N);
-    //für Rekonstruktion
-   
-
-    for (int i = 0; i < N; i++) {
-        hamming_normalization += hamming_window[i];
-    }
-
-    hamming_normalization /= N;
 
     // Normale Verarbeitung
     while (1) {
@@ -91,7 +83,7 @@ int main(void) {
             process_audio();
         }
     }
-}
+} 
 
 void process_audio() {
     uint32_t *rxbuf, *txbuf;
@@ -110,13 +102,14 @@ void process_audio() {
             //LMS_filter 
             float s_hat = a_hat * arm_cos_f32(omega *i) + b_hat * arm_sin_f32(omega*i);
             float error = filtered_sample - s_hat;
-            
+
             //LMS - aktualisieren 
             a_hat += STEP_SIZE * error * arm_cos_f32(omega * i);
             b_hat += STEP_SIZE * error * arm_sin_f32(omega * i);
 
             *txbuf++ = (int16_t)(error * 32768.0f);
-        }
+        } 
+
     }
 
     int available_data = (wrt_buf_ind - rd_buf_ind + BUFFER_SIZE) % BUFFER_SIZE;
@@ -127,12 +120,10 @@ void process_audio() {
             fft_input[i] = (float32_t)((int16_t)audio_ring_buffer[(rd_buf_ind + i) % BUFFER_SIZE]) / 32768.0f;
             mean += fft_input[i];
         }
-        rd_buf_ind = (rd_buf_ind + N) % BUFFER_SIZE;
-
+        //rd_buf_ind = (rd_buf_ind + N) % BUFFER_SIZE;
+        rd_buf_ind = (rd_buf_ind + HOP_SIZE) % BUFFER_SIZE; //update rd_ind um hop-size
         mean /= N;
 
-        
-        //DC entfernen 
         for (int i = 0; i < N; i++) {
             fft_input[i] = (fft_input[i] - mean) * hamming_window[i];
         }
@@ -160,7 +151,7 @@ void process_audio() {
             sum_spread += spread; 
 
             for (int i = 0; i < N / 2; i++) {
-                P_noise[i] += P_noisy[i] / NUM_NOISE_FRAMES;
+                P_noise[i] += noise_increment * P_noisy[i] / NUM_NOISE_FRAMES;
             }
             noise_cycle++;
 
@@ -186,35 +177,36 @@ void process_audio() {
             if (!filter_type){
                 //debug_printf("Wiener computed"); 
                 compute_wiener_filter();
-
-                // Apply Wiener filter on the FFT coefficients
                 for (int i = 0; i < N / 2; i++) {
-                    fft_output[2 * i] *= H[i];     // Apply Wiener filter to real part
-                    fft_output[2 * i + 1] *= H[i]; // Apply Wiener filter to imaginary part
+                    fft_output[2 * i] *= H[i];
+                    fft_output[2 * i + 1] *= H[i];
                 }
 
-                // Perform IFFT
                 arm_rfft_fast_f32(&fft_instance, fft_output, ifft_output, 1);
-
-                // Normalize the IFFT output by the hamming normalization factor
-                for (int i = 0; i < N; i++) {
-                    ifft_output[i] /= hamming_normalization;
+                for (int i = 0; i < HOP_SIZE; i++) {
+                    float32_t scaled_output=(ifft_output[i] + overlap_buffer[i]);
+                    out_ring_buffer[wrt_out_ind]= (int16_t)(scaled_output * 32768.0f);
+                    wrt_out_ind = (wrt_out_ind + 1) % BUFFER_SIZE;
                 }
 
-                // Handle overlap-add by adding the current and previous frames
-                // Make sure to only add the overlapping part
-                for (int i = 0; i < N / 2; i++) {
-                    ifft_output[i + N / 2] += previous_ifft_output[i + N / 2]/hamming_normalization; // Add the overlap
+                for (int i = 0; i < (N - HOP_SIZE); i++) {
+                    overlap_buffer[i] = ifft_output[i + HOP_SIZE];
                 }
-
-                // Save the current IFFT output for the next iteration (next frame overlap)
-                for (int i = 0; i < N / 2; i++) {
-                    previous_ifft_output[i] = ifft_output[i + N / 2]/hamming_normalization; // Store the second half for the next overlap
-                }
-
-                // Output the result (write to the output buffer)
-                for (int i = 0; i < N; i++) {
-                    *txbuf++ = (int16_t)(ifft_output[i] * 32768.0f);
+                // Ausgabe
+                //for (int i = 0; i < N; i++) {
+                    //*txbuf++ = (int16_t)(ifft_output[i] * 32768.0f);
+                //}
+                int available_data_output = (wrt_out_ind - rd_out_ind + BUFFER_SIZE) % BUFFER_SIZE;
+                if (available_data_output >= DMA_BUFFER_SIZE) {
+                    for (int i = 0; i < DMA_BUFFER_SIZE; i++) {
+                        if (rd_out_ind != wrt_out_ind) {
+                            *txbuf++ = (int16_t)(out_ring_buffer[rd_out_ind]);
+                            rd_out_ind = (rd_out_ind + 1) % BUFFER_SIZE;
+                        } 
+                        else {
+                            *txbuf++ = 0;  // Auffüllen mit Null
+                        }
+                    }
                 }
             }
 
@@ -228,10 +220,16 @@ void process_audio() {
 
 void compute_wiener_filter() {
     for (int i = 0; i < N / 2; i++) {
+        //P_clean[i] = P_noisy[i] - P_noise[i];
+        //P_clean[i] = fmaxf(P_noisy[i] - P_noise[i], 0.0f);
         P_clean[i] = P_noisy[i] - P_noise[i];
-        H[i] = P_clean[i] / (P_clean[i] + beta * P_noise[i]);
+        if (P_clean[i] > 0){
+            H[i] = P_clean[i] / (P_clean[i] + beta * P_noise[i]);
+            H[i] = powf(P_clean[i] / (P_clean[i] + beta * P_noise[i]), alpha);
+        } else {
+            H[i] = 0; 
+        }
     }
-
     // Beta Adaptiv ändern
     //if (sum_power > sum_power_noise){
         //beta = sum_power *10 / sum_power_noise;  
@@ -270,12 +268,4 @@ void generate_hamming_window(float *window, int size) {
     for (int i = 0; i < size; i++) {
         window[i] = 0.54 - 0.46 * arm_cos_f32(2 * PI * i / (size - 1));
     }
-}
-
-float32_t compute_SNR(float32_t signal_power,float32_t noise_power){
-    float SNR = 0.0f;
-
-    SNR = signal_power/noise_power;
-    return SNR;
-
 }
